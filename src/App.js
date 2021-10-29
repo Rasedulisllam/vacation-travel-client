3ac45ef8c30cdf5465baa3807ffead6f19c4343a
Rasedul <raselr699@gmail.com>
2021-10-29 11:41:37 +0600

    benner section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './pages/Header/Header';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Header></Header>
         <Switch>
-
+            <Home></Home>
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Home/Benner/Benner.js
@@ -0,0 +1,59 @@
+
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+import banner1 from '../../../images/benner/benner1.png'
+import banner2 from '../../../images/benner/benner2.png'
+import banner3 from '../../../images/benner/benner3.png'
+import { Button, Col, Container, Row } from 'react-bootstrap';
+
+const Benner = () => {
+    return (
+        <Carousel infiniteLoop autoPlay showThumbs={false}>
+                <div>
+                    <img src={banner1} alt='slider images' />
+                    <Container className="legend bg-transparent" style={{top:'30%'}} >
+                        <Row>
+                            <Col md={6} className='text-start ms-5'>
+                                <h1 className='fs-1  text-warning'>Find your parfect</h1>
+                                <h1 className='text-uppercase' style={{fontSize:'84px'}}>Vacation</h1>
+                                <p className='fs-5'>Bern, switzerland</p>
+                                <Button variant="outline-warning">Detail..</Button>
+                            </Col>
+                            <Col md={6}></Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div>
+                    <img src={banner2} alt='slider images' />
+                    <Container className="legend bg-transparent" style={{top:'30%'}} >
+                        <Row>
+                            <Col md={7} className='text-start ms-5 '>
+                                <h1 className='text-uppercase text-primary' style={{fontSize:'84px'}}>special</h1>
+                                <h1 className='text-uppercase fs-1' >5 days in italy</h1>
+                                <p className='fs-5'>Italy , Rome</p>
+                                <Button variant="primary">Detail..</Button>
+                            </Col>
+                            <Col md={5}></Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div>
+                    <img src={banner3} alt='slider images' />
+                    <Container className="legend bg-transparent" style={{top:'30%'}} >
+                        <Row>
+                            <Col md={6} className='text-start ms-5'>
+                                <h1 className='fs-1 text-warning text-uppercase'>Open your eyes to</h1>
+                                <h1 className='text-uppercase' style={{fontSize:'84px'}}>The hidden world</h1>
+                                <p className='fs-5'>Bern, switzerland</p>
+                                <Button variant="outline-warning">Detail..</Button>
+                            </Col>
+                            <Col md={6}></Col>
+                        </Row>
+                    </Container>
+                </div>
+        </Carousel>
+    );
+};
+
+export default Benner;
